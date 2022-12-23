b04f1ec24fd4a620b93b7c8eefe82c11b7b3caf5
Tinks18 <tinutani9@gmail.com>
2022-12-23 19:27:41 +0000

    Final commit

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const Paul = people.filter(person => person.name === 'Paul')[0];
+console.log(Paul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,12 @@ const students = [
     ]
   },
 ];
+
+const has5YearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5YearsExp).length > 0;
+
+const candidates = students.filter(hasStrongSkills);
+const names = candidates.map (student => student.name);
+
+console.log(candidates);
+console.log(names);
